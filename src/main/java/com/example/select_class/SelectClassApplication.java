2195bad6f7e608b02cc6b5712cede4998559cd44
汤万鹏 <walikrence@163.com>
2019-12-18 21:57:32 +0800

    init

---
+++ src/main/java/com/example/select_class/SelectClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.select_class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectClassApplication.class, args);
+    }
+
+}
